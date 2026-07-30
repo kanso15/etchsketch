be05448bfffaa8e186d51191016846effd02cbb8
kanso15 <kanso1515@gmail.com>
2026-07-30 23:55:13 +0300

    minor aesthetic changes + created user input function

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-let n = 16;
+const container = document.getElementById("container");
+
+
+const n = 16;
 function createSquare(n){
-    let l = Math.pow(n,2)
+    const l = Math.pow(n,2)
     for (i=l; i >0; i--){
-        const container = document.getElementById("container");
         const div = document.createElement("div");
         container.appendChild(div);
-        div.textContent = i;
         div.classList.add("square"); 
-        div.style.width = `${800 / n}px`
-        div.style.height = `${800 / n}px`
+        div.style.width = `${550 / n}px`
+        div.style.height = `${550 / n}px`
+    }
+}
+createSquare(n);
+
+function userGrid(){  
+    const input = document.getElementById('inputBox');
+    const userInput = input.value;
+
+    if(userInput>100 || userInput < 2){
+        alert("Please enter a number between 2 and 100")
+    }
+
+    else{
+        container.textContent = "";
+        createSquare(userInput);
     }
 }
 
-createSquare(n);
+
+const go = document.getElementById('createGrid');
+go.addEventListener('click', () => {userGrid()});
+
+go.addEventListener('hover', () => (hex()))
